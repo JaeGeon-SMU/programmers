54e7152f5bd633e51ccbb94efdb1c5cfc3021613
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-03-17 11:19:15 +0900

    ddd

--- pythonPractice/1934.py
+++ pythonPractice/1934.py
@@ -1,8 +1,23 @@
+def gcd(num1,num2):
+    r=1
+    if(num1 ==1 or num2 == 1):
+        return 1
+    
+    if(num1>num2):
+        while r!=0:
+            r=num1%num2
+            num1=num2
+            num2=r
+        return num1
+    else:
+        while r!=0:
+            r=num2%num1
+            num2=num1
+            num1=r
+        return num2
+
 a=int(input())
 for i in range(a):
     b,c=map(int,input().split())
-    d=1
-    while (d%b !=0) or (d%c!=0):
-        d+=1
-    print(d)
-    
+    print(b*c//gcd(b,c))
+
