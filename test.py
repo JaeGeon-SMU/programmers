8215978c1d6263d7797b5f352d561fdfac0c3b62
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-04-06 16:59:03 +0900

    aa

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello")
+a=[1, 1, 2, 2, 3, 3, 3]
+list=[]
+arr=set(a)
+
+for i in arr:
+    list.append(a.count(i))
+
+print(list.count(max(list)))
+
+if list.count(max(list))>=2:
+    answer= -1
+else:
+    answer= max(list)
+print(answer)
