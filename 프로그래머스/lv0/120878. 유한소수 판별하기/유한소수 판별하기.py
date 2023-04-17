bfcb799d185deacf085f8068b33fab384332b9a2
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-04-18 00:01:08 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,28 @@
+def solution(a, b):
+    answer = 1
+    a_list=soinsu(a)
+    b_list=soinsu(b)
+    c_list=soinsu(b)
+    
+    for i in b_list:
+        if i in a_list:
+            a_list.remove(i)
+            c_list.remove(i)
+            
+    
+    for j in c_list:
+        if j!=2 and j!=5:
+            answer=2
+            break
+    return answer
+
+def soinsu(q):
+    soinsu_list=[]
+    i=2
+    while q>1:
+        if q%i == 0 :
+            q=q//i
+            soinsu_list.append(i)
+        else:
+            i+=1
+    return soinsu_list
