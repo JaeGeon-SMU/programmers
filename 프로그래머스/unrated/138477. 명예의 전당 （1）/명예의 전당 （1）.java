360c65d340cd66b0b226b2853eda9cdae18c5b0d
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-13 11:21:28 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.47 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,18 +2,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> l = new ArrayList<>();
-        for(int i = 0; i<score.length ; i++){
-            if(l.size() == k){
-                if(Collections.min(l) < score[i]){
-                    l.remove(Collections.min(l));
-                    l.add(score[i]);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int i = 0 ; i<score.length; i++){
+            if(q.size() == k){
+                if(q.peek() < score[i]){
+                    q.poll();
+                    q.add(score[i]);
                 }
             }
-            else{
-                l.add(score[i]);
-            }
-            answer[i]=Collections.min(l);
+            else
+                q.add(score[i]);
+            answer[i]=q.peek();
         }
         return answer;
     }
