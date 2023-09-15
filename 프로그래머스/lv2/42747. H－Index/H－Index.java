ea6c89ec4ab9b1e9611ffd45f44ec37cc8cac0c7
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-15 16:01:33 +0900

    [level 2] Title: H-Index, Time: 0.42 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -5,18 +5,13 @@ class Solution {
         int cnt = citations.length;
         
         Arrays.sort(citations);
-        for(int k=0; k<=cnt; k++){
-            int a=0;
         for(int i=0; i<cnt; i++){
-            if(citations[i]>=answer)
-                a++;
+            int h = cnt-i;
+            if(citations[i]>=h){
+                answer = h;
+                break;
+            }
         }
-        if(a>=answer && cnt-a<=answer)
-            answer++;
-        else
-            break;
-    }
-        
-        return --answer;
+        return answer;
     }
 }
