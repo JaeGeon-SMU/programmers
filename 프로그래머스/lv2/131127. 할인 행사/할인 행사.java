c8fe0838a7009b66255e800ebdb366618a416617
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-18 12:22:16 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -6,11 +6,8 @@ class Solution {
         for(int i = 0 ; i<discount.length-9 ;i++){
             boolean flag = false;
             Map<String,Integer> map = new HashMap();
-            for(int j = i ; j<i+10; j++){
-                if(j>=discount.length)
-                    break;
-                map.put(discount[j],map.getOrDefault(discount[j],0)+1);
-            }
+            for(int j = i ; j<i+10; j++)
+                 map.put(discount[j],map.getOrDefault(discount[j],0)+1);
             for(int k = 0 ; k<want.length; k++){
                 if(map.getOrDefault(want[k],-1) != number[k]){
                     flag= false;
