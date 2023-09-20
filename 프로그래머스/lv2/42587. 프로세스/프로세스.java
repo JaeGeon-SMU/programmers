dce6122909219a2c10f28588ef4ad747430911fb
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-20 17:06:19 +0900

    [level 2] Title: 프로세스, Time: 0.72 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -2,18 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        PriorityQueue<Integer> q = new PriorityQueue((s1,s2) -> (int)s2-(int)s1);
-        for(int a: priorities)
-            q.add(a);
-        
-        while(!q.isEmpty()){
+        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+        for(int a : priorities)
+            pq.add(a);
+        while(!pq.isEmpty()){
             for(int i = 0; i<priorities.length; i++){
-                if(q.peek() == priorities[i]){
+                if(pq.peek()==priorities[i]){
                     if(i==location){
                         return ++answer;
                     }
-                    q.poll();
                     answer++;
+                    pq.poll();
                 }
             }
         }
