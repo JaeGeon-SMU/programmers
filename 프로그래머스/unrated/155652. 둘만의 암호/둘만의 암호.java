820cf13805f55dc56d8b792540510eaff98817fa
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-22 18:33:00 +0900

    [unrated] Title: 둘만의 암호, Time: 0.10 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -2,23 +2,20 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
-        int j =0; int aa=0;
+        int aa=0;
         char c ;
         for(int i = 0; i<s.length(); i++){
             c = s.charAt(i);
             int idx = index;
-            for(j = 1 ; j<=idx; j++){
+            for(int j = 1 ; j<=idx; j++){
                 aa = c+j;
                 while(aa>122)
                     aa-=26;
                 if(skip.indexOf((char)aa)!=-1)
                     idx++;
-
-            
             }
             sb.append((char)aa);
         }
-        
         return sb.toString();
     }
 }
