5eeda91bfa37d0f99de3e6223a2151c634c93b1c
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-22 15:11:48 +0900

    [unrated] Title: 옹알이 (2), Time: 3.01 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -2,12 +2,11 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(String s : babbling){
-            s=s.replaceAll("ayaaya|yeye|woowoo|mama","0");
+            s=s.replaceAll("ayaaya|yeye|woowoo|mama"," ");
             s=s.replaceAll("aya|ye|woo|ma","");
             if(s.equals(""))
                 answer++;
         }
-        
         return answer;
     }
 }
