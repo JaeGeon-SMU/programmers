d569fc78e5d69860840b3f661c27326e47bf4e96
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-09-25 17:37:15 +0900

    [unrated] Title: 달리기 경주, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String,Integer> map = new HashMap();
+        
+        for(int i = 0 ; i<players.length; i++){
+            map.put(players[i],i);
+        }
+        for(String s : callings){
+            int idx = map.get(s);
+            players[idx] = players[idx-1];
+            players[idx-1] = s;
+            map.put(players[idx],map.get(s));
+            map.put(players[idx-1],map.get(s)-1);
+        }
+        
+        return players;
+    }
+}
