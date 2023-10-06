a69e558f51e4b9567cfec14ac835c6e211650926
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-10-06 12:11:50 +0900

    [level 2] Title: 타겟 넘버, Time: 0.40 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,17 @@
 class Solution {
-    int answer=0;
+    int answer = 0;
     public int solution(int[] numbers, int target) {
-        int sum = 0;
-        dfs(numbers,sum,target,0);
+        dfs(numbers,target,0,0);
         return answer;
     }
-    public void dfs(int[] numbers, int sum ,int target,int idx){
-        if(idx == (numbers.length-1)){
-            if(sum+numbers[idx]==target)
-                answer++;
-            else if( sum-numbers[idx]==target)
+    public void dfs(int[] numbers,int target,int idx,int sum){
+        if(numbers.length-1==idx){
+            if(sum+numbers[idx]==target||sum-numbers[idx]==target)
                 answer++;
         }
         else{
-            dfs(numbers,sum+numbers[idx],target,idx+1);
-            dfs(numbers,sum-numbers[idx],target,idx+1);
-        }
+            dfs(numbers,target,idx+1,sum+numbers[idx]);
+            dfs(numbers,target,idx+1,sum-numbers[idx]);
+        }        
     }
 }
