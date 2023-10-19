f83761bf0c0335595f1635d494e0a279eb7d4757
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-10-19 10:17:04 +0900

    [level 2] Title: 땅따먹기, Time: 17.62 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,21 +1,23 @@
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
+    
         int[][] sum = new int[land.length+1][land[0].length];
-        
-        for(int i = 1 ; i<sum.length; i++){
-            for(int j = 0; j<sum[i].length;j++){
-                int addNum = 0;
-                for(int k = 0; k<land[0].length; k++){
-                    if(k!=j){
-                        addNum = Math.max(addNum,sum[i-1][k]);
-                    }
-                    sum[i][j] = addNum + land[i-1][j];
+        for(int i = 1; i<sum.length; i++ ){
+            for(int j = 0; j<sum[0].length; j++){
+                int maxSum = 0;
+                for(int k = 0; k<land[0].length ; k++){
+                    if(j!=k)
+                        maxSum = Math.max(maxSum,sum[i-1][k]); 
                 }
+                sum[i][j] +=(maxSum+land[i-1][j]); 
             }
-        } 
-        for(int u = 0; u<land[0].length; u++)
-            answer = Math.max(answer,sum[land.length][u]);
+        }
+        
+        for(int a = 0; a<sum[0].length; a++){
+            answer = Math.max(answer,sum[land.length][a]);
+        }
+        
         return answer;
     }
 }
