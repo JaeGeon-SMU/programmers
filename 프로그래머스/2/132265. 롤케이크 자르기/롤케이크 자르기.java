f4934493c57c7ed2c7910d1bbbba0e95a5f3a6d1
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-10-25 10:12:47 +0900

    [level 2] Title: 롤케이크 자르기, Time: 103.85 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,16 +4,16 @@ class Solution {
         int answer = 0;
         Set<Integer> s1 = new HashSet();
         Set<Integer> s2 = new HashSet();
-        int[] s1Arr = new int[topping.length];
-        int[] s2Arr = new int[topping.length];
-        for(int i = 0 ; i<topping.length; i++){
+        int[] arr1 = new int[topping.length];
+        int[] arr2 = new int[topping.length];
+        for(int i = 0; i<topping.length; i++){
             s1.add(topping[i]);
             s2.add(topping[topping.length-1-i]);
-            s1Arr[i] = s1.size();
-            s2Arr[topping.length-1-i] = s2.size();
+            arr1[i]=s1.size();
+            arr2[topping.length-1-i]=s2.size();
         }
-        for(int i = 0; i<topping.length-1; i++){
-            if(s1Arr[i]==s2Arr[i+1])
+        for(int i =0; i<topping.length-1; i++){
+            if(arr1[i]==arr2[i+1])
                 answer++;
         }
         return answer;
