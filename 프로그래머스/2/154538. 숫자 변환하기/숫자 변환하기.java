fb7e20c0787951c0a33d7cd5ec06bde8526e9594
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-10-25 10:29:05 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.75 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,27 +1,34 @@
 import java.util.*;
 class Solution {
-    int answer= -1;
     Queue<Integer[]> q = new LinkedList();
+    int answer = -1;
     public int solution(int x, int y, int n) {
         q.add(new Integer[]{y,0});
         bfs(x,n);
         return answer;
     }
-    void bfs(int x,int n){        
+    void bfs(int x,int n){
         while(!q.isEmpty()){
-            Integer[] a = q.poll();
-            if(a[0]==x){
-                answer = a[1];  
-                break;
+            Integer[] arr = q.poll();
+            if(arr[0]==x){
+                answer=arr[1];
+                return ;
             }
-            if(a[0]%3==0)
-                q.add(new Integer[]{a[0]/3,a[1]+1});
-            if(a[0]%2==0)
-                q.add(new Integer[]{a[0]/2,a[1]+1});
-            if(a[0]-n>=x)
-                q.add(new Integer[]{a[0]-n,a[1]+1});
+            else{
+                if(arr[0]%3==0){
+                    answer++;
+                    q.add(new Integer[]{arr[0]/3,arr[1]+1});
+                }
+                if(arr[0]%2==0){
+                    answer++;
+                    q.add(new Integer[]{arr[0]/2,arr[1]+1});
+                }
+                if(arr[0]-n>=x){
+                    answer++;
+                    q.add(new Integer[]{arr[0]-n,arr[1]+1});
+                }
+            }
         }
+        answer =-1;
     }
-    
-    
 }
