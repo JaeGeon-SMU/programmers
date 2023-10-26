2ca94511701776b883f2742ae124aa359586cbea
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-10-26 09:50:48 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.90 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -4,8 +4,8 @@ class Solution {
         arr[1]=1;
         arr[2]=2;
         for(int i = 3; i<arr.length; i++){
-            arr[i]= (arr[i-2]+arr[i-1])%1000000007;
+            arr[i]=(arr[i-2]+arr[i-1])%1000000007;
         }
-        return arr[arr.length-1];
+        return arr[n];
     }
 }
