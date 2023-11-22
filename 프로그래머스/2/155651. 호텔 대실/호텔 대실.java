b7b4456ec17e1d2de820ea7ffbc823809b411594
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-11-22 10:17:55 +0900

    [level 2] Title: 호텔 대실, Time: 5.20 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        int[] arr = new int[60*24+10];
+        int[] arr = new int[1450];
         for(String[] s : book_time){
-            arr[Integer.parseInt(s[0].split(":")[0])*60 + Integer.parseInt(s[0].split(":")[1])]++;
-            arr[Integer.parseInt(s[1].split(":")[0])*60 + Integer.parseInt(s[1].split(":")[1])+10]--;
+            arr[Integer.valueOf(s[0].split(":")[0])*60+Integer.valueOf(s[0].split(":")[1])]++;
+            arr[Integer.valueOf(s[1].split(":")[0])*60+Integer.valueOf(s[1].split(":")[1])+10]--;
         }
         for(int i = 1 ; i<arr.length; i++){
-            arr[i]+=arr[i-1];
-            answer=Math.max(arr[i],answer);
+            arr[i] +=arr[i-1];
+            answer=Math.max(answer,arr[i]);
         }
         return answer;
     }
