eae60a1d377ad982e64a94a47800fbc33341c802
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-04-08 14:46:32 +0900

    [Gold IV] Title: 좋다, Time: 376 ms, Memory: 22040 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,33 +1,32 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i<n; i++)
-            arr[i]=sc.nextInt();
-        Arrays.sort(arr);
         int ans =0;
-        for(int i =0;i<n;i++){
-            int start = 0;
+        int[] arr = new int[n];
+        for(int i =0 ; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            int start =0;
             int end = n-1;
             int find = arr[i];
             while(start<end){
                 if(arr[start]+arr[end]==find){
-                    if(i!=start && i!=end){
+                    if(start==i)
+                        start++;
+                    else if(end ==i)
+                        end--;
+                    else {
                         ans++;
                         break;
                     }
-                    else if( i==start)
-                        start++;
-                    else
-                        end--;
                 }
-                else if( arr[start] + arr[end]<find){
+                else if(arr[start]+arr[end]<find)
                     start++;
-                }
-                else
+                else if(arr[start]+arr[end]>find)
                     end--;
             }
         }
