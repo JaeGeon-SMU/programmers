6408f2b99842411c8269b2bc09dd422d85c4c7bc
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2025-03-20 12:37:23 +0900

    [level 2] Title: 기능개발, Time: 0.67 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,36 +1,30 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<Integer>();
-        Queue<Integer> p_q = new LinkedList<Integer>();
-        Queue<Integer> s_q = new LinkedList<Integer>();
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
 
-        for(int i = 0 ; i<progresses.length; i++){
-            p_q.add(progresses[i]);
-            s_q.add(speeds[i]);
-        }
-        int day =1;
-        int cnt =0;
-        while(!p_q.isEmpty()){
-            int p = p_q.peek();
-            int s = s_q.peek();
-            if((p+day*s)<100){
-                day++;
-                if(cnt!=0){
-                    list.add(cnt);
-                    cnt=0;
-                }
-            }else{
-                p_q.poll();
-                s_q.poll();
-                cnt++;
+        for (int i = 0; i < speeds.length; i++) {
+            double remain = (100 - progresses[i]) / (double) speeds[i];
+            int date = (int) Math.ceil(remain);
+
+            if (!q.isEmpty() && q.peek() < date) {
+                answerList.add(q.size());
+                q.clear();
             }
+
+            q.offer(date);
         }
-        list.add(cnt);
-        int[] arr = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            arr[i]=list.get(i);
+
+        answerList.add(q.size());
+
+        int[] answer = new int[answerList.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
         }
-        return arr;
+
+        return answer;
     }
 }
