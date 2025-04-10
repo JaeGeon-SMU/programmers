416f056aecbb2fea29addfc4f0bb0ba2424fb579
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2025-04-10 14:23:39 +0900

    [level 1] Title: 덧칠하기, Time: 2.06 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,18 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        Set<Integer> s = new HashSet<>();
-        for(int i : section) s.add(i);
+        int answer = 1;
         int pre = 1;
-        for(int i = 0; i<=n; i++){
-            for(int j = pre; j<pre+m; j++){
-                if(s.contains(i)){
-                    answer++;
-                    i=i+m-1;
-                    break;
-                }
+        for(int i =1; i<section.length; i++){
+            if(pre+m-1<section[i]){
+                answer++;
+                pre=section[i];
             }
         }
         return answer;
