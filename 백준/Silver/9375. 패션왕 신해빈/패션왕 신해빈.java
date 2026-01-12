5ae227427f7b2eee087e6115438f5be0e6be3bbd
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2026-01-12 17:00:02 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 112 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,26 +1,36 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int cnt = sc.nextInt(); // n번 시행
-        
-        for(int i=0; i<cnt; i++){
-            int cloth = sc.nextInt(); // 해당 시행 옷 개수
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); // 테스트 횟수
+
+        for (int i = 0; i < n; i++) {
+            int cnt = Integer.parseInt(br.readLine()); // 의류 개수
+
+            Map<String, Integer> clothes = new HashMap<>();
             int ans = 1;
-            Map<String,Integer> clothes = new HashMap<String,Integer>();
-            
-            for(int j = 0; j<cloth; j++){
-                String clothName = sc.next();
-                String clothType = sc.next();
-                clothes.put(clothType,clothes.getOrDefault(clothType,1)+1);
+
+            for (int j = 0; j < cnt; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String type = st.nextToken();
+
+                clothes.put(type, clothes.getOrDefault(type, 1) + 1);
             }
-           
-            for(Integer val : clothes.values()){
-                ans*=val;
+
+            for (int v : clothes.values()) {
+                ans *= v;
             }
-            System.out.println(ans-1);
+
+            System.out.println(ans - 1);
         }
     }
 }
